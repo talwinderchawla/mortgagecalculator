276b852cb4c8409336f8475ced7247fbb724b025
Talwinder Chawla <talwinderchawla@Talwinders-MacBook-Pro.local>
2022-11-09 11:26:51 -0500

    Header changes

--- gcmortgagecalculator/src/app/app.module.ts
+++ gcmortgagecalculator/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { MortgageCalculatorComponent } from './components/mortgage-calculator/mortgage-calculator.component';
+import { MaterialLibsModule } from './commons/material-libs/material-libs.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    MortgageCalculatorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialLibsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
